25844b2be241b6cdd8ec5a340ce08a44dce9bd83
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-04 18:50:03 -0500

    stub preference methods

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,3 +1,15 @@
 class Preference
-  
+    def artist_sort_order
+    end
+
+    def song_sort_order
+    end
+
+    def allow_create_artists
+    end
+
+    def allow_create_songs
+    end
+
+
 end
