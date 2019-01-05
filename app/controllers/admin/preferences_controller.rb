b9f0f2fc02ae77dbd32efdb2f2601b8bccd3336c
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-05 12:28:43 -0500

    implement prefs#update

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -3,7 +3,14 @@ class Admin::PreferencesController < ApplicationController
     @preferences = Preference.first_or_create(artist_sort_order: "ASC", song_sort_order: "DESC", allow_create_artists: true, allow_create_songs: true)
   end
 
-  def edit
-    
+  def update
+    Preference.find(params[:id]).update(preference_params)
+    redirect_to songs_path
+  end
+
+  private
+
+  def preference_params
+    params.require(:preference).permit(:artist_sort_order, :song_sort_order, :allow_create_artists, :allow_create_songs)
   end
 end
