cb0ca4d53919b04bfa979d080aa20db3418da852
Indre Bakeer <indre.bakeer@gmail.com>
2019-09-16 03:37:38 +0000

    Done.

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,4 +1,6 @@
 class Preference < ActiveRecord::Base
-
-
+  validates :allow_create_artists, inclusion: {in: ["ASC", "DESC"]}
+  validates :allow_create_artists, inclusion: {in: ["ASC", "DESC"]}
+  validates :allow_create_artists, inclusion: {in: [true, false]}
+  validates :allow_create_songs, inclusion: {in: [true, false]}
 end
