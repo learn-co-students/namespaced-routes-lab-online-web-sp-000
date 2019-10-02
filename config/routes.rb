b71446babb15523c2c728ce769725a294d662694
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-10-02 20:13:33 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :preferences
+  namespace :admin do
+    resources :preferences, only: [:index]
+  end
+
   resources :artists do
     resources :songs, only: [:index, :show]
   end
