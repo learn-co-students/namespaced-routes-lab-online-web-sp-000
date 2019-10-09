89fa2a16ddabc31af1b7fc691067d08d9da203ff
chanwkkk <chanwk18@hotmail.com>
2019-10-09 17:06:11 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
 class SongsController < ApplicationController
+  before_action :set_preferences, only: [:index,:new]
   def index
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
@@ -25,7 +26,11 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if @preference && @preference.allow_create_artists
+      @song = Song.new
+    else
+      redirect_to songs_path
+    end
   end
 
   def create
@@ -65,6 +70,10 @@ class SongsController < ApplicationController
 
   def song_params
     params.require(:song).permit(:title, :artist_name)
+  end
+
+  def set_preferences
+    @preference = Preference.first
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-
-  resources :preferences
+  namespace :admin do
+    resources :preferences
+  end
   resources :artists do
     resources :songs, only: [:index, :show]
   end
