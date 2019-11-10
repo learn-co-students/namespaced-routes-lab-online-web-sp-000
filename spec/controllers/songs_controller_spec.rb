d23a6823f6f5ddfd6200d1f82d6496441f63c62d
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-11-09 22:25:13 -0800

    Done.

--- spec/controllers/songs_controller_spec.rb
+++ spec/controllers/songs_controller_spec.rb
@@ -22,10 +22,10 @@ RSpec.describe SongsController do
     end
 
   end
-  
+
   describe "GET new" do
     it "redirects when access is turned off" do
-      p = Preference.create(allow_create_songs: false)
+      p = Preference.create(allow_new_songs: false)
       get :new
       expect(response).to redirect_to songs_path
     end
