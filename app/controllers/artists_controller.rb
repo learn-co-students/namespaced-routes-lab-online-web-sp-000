99f0babd1d95f9b3ef7b488a79f95d784776bb59
Ronnie Stephens <rstephens@bluemorpho.cc>
2020-01-28 12:42:39 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,7 +8,12 @@ class ArtistsController < ApplicationController
   end
 
   def new
+    @preference = Preference.first
+    if @preference.allow_create_artists
     @artist = Artist.new
+    else
+      redirect_to artists_path
+    end
   end
 
   def create
@@ -49,4 +54,5 @@ class ArtistsController < ApplicationController
   def artist_params
     params.require(:artist).permit(:name)
   end
+
 end
