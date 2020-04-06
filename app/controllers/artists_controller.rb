8eb2bd4ebd04ae915e859429839b8e5d9e1fb8ad
RickLearnsToCode <rick.rosetti@gmail.com>
2020-04-05 21:58:12 -0600

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,7 +8,12 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    if Preference.first.allow_create_artists == true
+      @artist = Artist.new
+    else
+      redirect_to artists_path, alert: "Cannot create new artists"
+    end
+
   end
 
   def create
