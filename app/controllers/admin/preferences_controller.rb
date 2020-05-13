c023e3a02269522a06304e6718bc096a06775b5c
nathan-stamand <nathansaint810@gmail.com>
2020-05-13 14:59:31 -0500

    Done.

---
+++ app/controllers/admin/preferences_controller.rb
@@ -0,0 +1,20 @@
+class Admin::PreferencesController < ApplicationController 
+    def index
+        # render layout: 'layout/application'
+    end
+
+    def new
+    end
+
+    def create
+    end 
+
+    def edit
+    end
+
+    def update
+    end 
+
+    def show
+    end
+end
