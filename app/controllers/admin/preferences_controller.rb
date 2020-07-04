37bc0308942e3b1529096f8fac3f69d63e9cb56b
gabejoseph <gabriel.joseph4@gmail.com>
2020-07-04 13:31:10 -0400

    Done.

---
+++ app/controllers/admin/preferences_controller.rb
@@ -0,0 +1,22 @@
+class Admin::PreferencesController < ApplicationController
+    
+    def index
+    end
+  
+    # def show
+    #   @artist = Artist.find(params[:id])
+    # end
+  
+    # def new
+    #   @artist = Artist.new
+    # end
+  
+
+  
+    # private
+  
+    # def artist_params
+    #   params.require(:artist).permit(:name)
+    # end
+  end
+  
