f5922124fb57d211b04b3f2481fde6ecf7548379
connorworthen <connorw220@protonmail.com>
2020-07-23 13:36:04 -0700

    Done.

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,2 +1,11 @@
 class Preference < ActiveRecord::Base
+
+  def self.no_access_artists
+    self.create(allow_create_artists: false)
+  end
+
+  def self.no_access_songs
+      self.create(allow_create_songs: false)
+  end
+
 end
