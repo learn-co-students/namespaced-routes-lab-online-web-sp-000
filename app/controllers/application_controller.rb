47898f7b6bb079690de4e581b3a929eeee412bd1
connorworthen <connorw220@protonmail.com>
2020-07-23 13:37:02 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def preference
-    Preference.find_by(id: 1) || Preference.new({
-      artist_sort_order: "ASC",
-      song_sort_order: "ASC",
-      allow_create_songs: false,
-      allow_create_artists: false
-    })
-  end
 end
