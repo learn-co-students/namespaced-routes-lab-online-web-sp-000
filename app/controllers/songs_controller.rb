77dd9f793cf0757148a7e515a9aa6a0a9df7a351
Elliot Blanchard <git@elliotblanchard.com>
2020-08-08 19:42:23 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,13 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    @preference = Preference.first
+    
+    if @preference.allow_create_artists == true
+      @song = Song.new
+    else
+      redirect_to songs_url
+    end    
   end
 
   def create
