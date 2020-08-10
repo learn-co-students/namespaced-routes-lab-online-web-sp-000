3a10b77a72965e8af1fa5b03bceac27e058601d1
russell-macalintal <russell.macalintal@gmail.com>
2020-08-10 00:28:31 -0400

    Done.

---
+++ app/controllers/admin/preferences_controller.rb
@@ -0,0 +1,5 @@
+class Admin::PreferencesController < ApplicationController
+    def index
+        @pref = Preference.first
+    end
+end
---
+++ app/helpers/preferences_helper.rb
@@ -0,0 +1,9 @@
+module PreferencesHelper
+    def check_artist_permission
+        
+    end
+
+    def check_song_permission
+        
+    end
+end
