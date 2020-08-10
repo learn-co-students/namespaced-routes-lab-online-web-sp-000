105d2a0caf9ddc3150e160deb95c5840364ab812
ting682 <tchung682@gmail.com>
2020-08-10 16:42:28 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,7 +8,14 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    #binding.pry
+    if Preference.all.first.allow_create_artists
+      @artist = Artist.new
+
+    else
+      redirect_to artists_path
+    end
+    
   end
 
   def create
