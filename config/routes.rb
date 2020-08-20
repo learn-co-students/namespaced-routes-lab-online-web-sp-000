f7f935903e4efadc77ac1fc95bd33b41ea24e279
cantstopcoding <jonathan_koss@yahoo.com>
2020-08-19 21:51:20 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,15 @@ Rails.application.routes.draw do
   resources :artists do
     resources :songs, only: [:index, :show]
   end
+
+  namespace :admin do 
+    resources :preferences
+  end
+
   resources :songs
 
 end
+
+# scope '/admin', module: 'admin' do
+#   resources :stats, only: [:index]
+# end
