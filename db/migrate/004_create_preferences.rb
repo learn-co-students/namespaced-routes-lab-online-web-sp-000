f9ed124c9db445ad794d3e1890e085a28c6936ab
Olivia Cubela <oliviacubela@gmail.com>
2020-11-11 15:21:34 -0500

    Done.

---
+++ db/migrate/004_create_preferences.rb
@@ -0,0 +1,10 @@
+CreatePreferences < ActiveRecord::Migration[4.2]
+  def change 
+    create_table :preferences do |t|
+      t.boolean :allow_create_artists 
+      t.boolean :allow_create_songs 
+
+      t.timestamps 
+    end
+  end
+end
