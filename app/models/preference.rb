e088aae915edecb7477abf672130d4575bfc82f8
kkirby16 <kkirby16@my.whitworth.edu>
2021-01-27 20:49:20 -0800

    Done.

---
+++ app/models/preference.rb
@@ -0,0 +1,18 @@
+class Preference < ActiveRecord::Base
+  #   attr_accessor :allow_create_artists, :allow_create_songs
+
+  #   def initialize(allow_create_artists, allow_create_songs)
+  #     @allow_create_artists = allow_create_artists
+  #     @allow_create_songs = allow_create_songs
+  #   end
+end
+
+#require 'rails_helper'
+
+# RSpec.describe Preference, type: :model do
+#   it "has the right fields" do
+#     p = Preference.new(allow_create_artists: false, allow_create_songs: true)
+#     expect(p.allow_create_artists).to eq false
+#     expect(p.allow_create_songs).to eq true
+#   end
+# end
