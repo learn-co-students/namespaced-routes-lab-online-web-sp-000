13b4a55d9a74f122b7282e574d7e4b61e8a0f097
taroj khan <tarujkhan@yahoo.com>
2021-02-18 19:32:50 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  resources :artists do
+    resources :songs, only: [:index, :show, :new]
+  end
+
+  resources :songs
 
   namespace :admin do
     resources :preferences, only: [:index, :show, :new]
   end
-  resources :songs
+  #resources :songs
 
 end
