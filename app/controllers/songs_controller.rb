a257fb0ed68d7a5c4da1c4dd2f452141107f08d7
acagirici <acagirici23@gmail.com>
2021-02-25 01:00:47 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,6 +26,11 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    if Preference.first.allow_create_songs == true
+      @song = Song.new
+    else
+      redirect_to songs_path
+    end
   end
 
   def create
