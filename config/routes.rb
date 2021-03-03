70fdea7549804c0eead5d1a8d21caeaabb813f4c
nlanese21 <nlanese21@gmail.com>
2021-03-03 17:34:57 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
   resources :artists do
     resources :songs, only: [:index, :show]
   end
+
+  namespace :admin do
+    resources :preferences do
+      resources :songs, only: [:new, :create]
+      resources :artists, only: [:new, :create]
+    end
+  end
+
   resources :songs
 
 end
