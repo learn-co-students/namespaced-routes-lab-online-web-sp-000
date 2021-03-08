4b13dedb01cbe3bef890584e800bab4e69f122df
RSigmaSG <sg.comparch@gmail.com>
2021-03-08 18:06:20 -0500

    Done.

--- db/migrate/20210308224317_create_preferences.rb
+++ db/migrate/20210308224317_create_preferences.rb
@@ -1,8 +1,7 @@
 class CreatePreferences < ActiveRecord::Migration[5.0]
   def change
     create_table :preferences do |t|
-      t.string :artist_sort_order
-      t.string :song_sort_order
+
       t.boolean :allow_create_songs
       t.boolean :allow_create_artists
 
--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,6 @@ ActiveRecord::Schema.define(version: 20210308224317) do
   end
 
   create_table "preferences", force: :cascade do |t|
-    t.string   "artist_sort_order"
-    t.string   "song_sort_order"
     t.boolean  "allow_create_songs"
     t.boolean  "allow_create_artists"
     t.datetime "created_at",           null: false
