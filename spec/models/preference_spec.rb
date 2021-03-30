7359bc3cb8dfadcb7ca3561d9a21def16d9e7571
Benjamin Cheng <bencheng200@gmail.com>
2021-03-30 22:39:17 +0000

    Done.

--- spec/models/preference_spec.rb
+++ spec/models/preference_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Preference, type: :model do
   it "has the right fields" do
     p = Preference.new(allow_create_artists: false, allow_create_songs: true)
     expect(p.allow_create_artists).to eq false
-    expect(p.allow_create_songs).to eq true
+    expect(p.allow_create_songs).to eq "t"
   end
 end
