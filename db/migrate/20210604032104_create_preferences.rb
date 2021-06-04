15b997f6ce470da575f6e1fee5618896ff1d81ba
JustinBreneman <Justin.Breneman@yahoo.com>
2021-06-03 20:41:14 -0700

    Done.

---
+++ db/migrate/20210604032104_create_preferences.rb
@@ -0,0 +1,8 @@
+class CreatePreferences < ActiveRecord::Migration[5.0]
+  def change
+    create_table :preferences do |t|
+      t.boolean :allow_create_songs, default: :true
+      t.boolean :allow_create_artists, default: :true
+    end
+  end
+end
